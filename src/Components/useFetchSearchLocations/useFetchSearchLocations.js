e94e0b38233822b7da3d50a86b5745df8dc19107
dzewelina <ewelina.zukowska26@gmail.com>
2022-01-25 14:26:06 +0000

    Debounce request

--- src/Components/useFetchSearchLocations/useFetchSearchLocations.js
+++ src/Components/useFetchSearchLocations/useFetchSearchLocations.js
@@ -1,32 +1,18 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import fetchLocations from "../../Functions/fetchLocations";
+import useDebounce from "../../Functions/useDebounce";
 import { text } from "../../consts";
 
 const useFetchSearchLocations = (searchText) => {
   const maxNumberOfLoc = 6;
-  const delay = 1000;
+  const delay = 500;
+
+  const debouncedSearchText = useDebounce(searchText, delay);
   const [places, setPlaces] = useState([]);
 
-  //   const debouncedFetch = useCallback(debounce(fetchLocations, delay), []);
-
   useEffect(() => {
-    if (searchText && searchText.length > 1) {
-      //   console.log(
-      //     "I AM HERE"
-      //   searchText,
-      //   "I AM FETCHING",
-      //   debouncedFetch(searchText, maxNumberOfLoc)
-      //   );
-
-      //   const calls = [];
-
-      //   calls.push(debouncedFetch(searchText, maxNumberOfLoc));
-      //   setTimeout(() => {
-      //     calls.push(debouncedFetch(searchText, maxNumberOfLoc)) &&
-      //       console.dir(calls);
-      //   }, 3000);
-
-      fetchLocations(searchText, maxNumberOfLoc).then((data) => {
+    if (debouncedSearchText && debouncedSearchText.length > 1) {
+      fetchLocations(debouncedSearchText, maxNumberOfLoc).then((data) => {
         if (data[0].name === "No results found") {
           setPlaces([{ name: text.noResults, placeKey: "no-results" }]);
         } else {
@@ -36,7 +22,7 @@ const useFetchSearchLocations = (searchText) => {
     } else {
       setPlaces([]);
     }
-  }, [searchText]);
+  }, [debouncedSearchText]);
 
   return places;
 };
--- src/Components/useFetchSearchLocations/useFetchSearchLocations.test.js
+++ src/Components/useFetchSearchLocations/useFetchSearchLocations.test.js
@@ -1,37 +1,49 @@
 import { renderHook } from "@testing-library/react-hooks";
 import useFetchSearchLocations from "../useFetchSearchLocations/useFetchSearchLocations";
-import fetchLocations from "../../Functions/fetchLocations";
-
-jest.mock("../../Functions/fetchLocations");
+import * as fetchLocations from "../../Functions/fetchLocations";
+import * as useDebounce from "../../Functions/useDebounce";
 
 describe("useFetchSearchLocations", () => {
+  let mockedFetchLocations;
+
   beforeEach(() => {
-    jest.clearAllMocks();
+    jest.spyOn(fetchLocations, "default");
+    mockedFetchLocations = fetchLocations.default;
   });
+
+  afterEach(() => jest.clearAllMocks());
 
   it("should not call api if location not given", () => {
     renderHook(() => useFetchSearchLocations());
-    expect(fetchLocations).not.toHaveBeenCalled();
+    expect(mockedFetchLocations).not.toHaveBeenCalled();
   });
 
   it("should not call api with single alphanumeric characters", () => {
     renderHook(() => useFetchSearchLocations("m"));
-    expect(fetchLocations).not.toHaveBeenCalled();
+    expect(mockedFetchLocations).not.toHaveBeenCalled();
   });
 
   it("should call api with 2 alphanumeric characters", () => {
     renderHook(() => useFetchSearchLocations("ma"));
-    expect(fetchLocations).toHaveBeenCalled();
+    expect(mockedFetchLocations).toHaveBeenCalled();
   });
 
   it("should call api with 2 or more alphanumeric characters", () => {
     renderHook(() => useFetchSearchLocations("manchester"));
-    expect(fetchLocations).toHaveBeenCalled();
+    expect(mockedFetchLocations).toHaveBeenCalled();
   });
 
   it("should call api requesting 6 results", () => {
     renderHook(() => useFetchSearchLocations("manchester"));
-    expect(fetchLocations).toHaveBeenCalledWith("manchester", 6);
+    expect(mockedFetchLocations).toHaveBeenCalledWith("manchester", 6);
+  });
+
+  it("should debounce reguested  search text", () => {
+    jest.spyOn(useDebounce, "default");
+    const mockedUseDebounce = useDebounce.default;
+    renderHook(() => useFetchSearchLocations("manchester"));
+
+    expect(mockedUseDebounce).toHaveBeenCalledWith("manchester", 500);
   });
 
   //   it("should save message when input is not recognised", () => {
